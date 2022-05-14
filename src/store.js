2e0d33cbb1687bb3e1f189c602cbbea83df62b2a
Kinder <nguyenductrong12a1@gmail.com>
2022-05-14 16:26:30 +0700

    Add middleware

--- src/store.js
+++ src/store.js
@@ -1,7 +1,21 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from 'services/reducers/';
+import { CREATE_TASK } from 'services/actions/task'
 
+const replaceMiddleware = store => next => action => {
+    const invalidWords = ["fuck", "kill"];
+    if (action.type === CREATE_TASK) {
+        action.payload.task = action.payload.task
+            .split(" ")
+            .map(word => invalidWords.includes(word) ? "***" : word)
+            .join(" ");
+    }
+    return next(action);
+}
 
-const store = createStore(rootReducer);
+const store = createStore(
+    rootReducer,
+    applyMiddleware(replaceMiddleware),
+);
 
 export default store;
